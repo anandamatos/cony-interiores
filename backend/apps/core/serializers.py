380caa9f59468d284a712a485c3e7ee7bef04eba
Matheus <matheusgr2022@outlook.com>
2026-07-01 17:09:07 -0300

    Revert "Feat/+ models e core"

--- backend/apps/core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Tarefa
-
-#Transforma dados do banco em JSON e vice-versa
-class TarefaSerializer(serializers.ModelSerializer):
-    class Meta:#regras do tradutor:
-        model = Tarefa #esse serializer trabalha com a tabela Tarefa
-        fields = '__all__' #pega todos os campos da tabela
--- backend/apps/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('tarefas/', views.listar_tarefas)
-]
