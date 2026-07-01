243f3f70154852f4dca1b5fa7267f8f0a42a0e05
Gabriel Augusto <gabrielaugusto873@yahoo.com.br>
2026-06-30 22:57:44 -0300

    TASK-M1-UX-006: Criar página de listagem de costureiras

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Costureiras from "../pages/Costureiras";
+import CadastroCostureira from "../pages/CadastroCostureiras";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/costureiras" element={<Costureiras />} />
+        <Route path="/costureiras/nova" element={<CadastroCostureira />} />
       </Routes>
     </BrowserRouter>
   );
