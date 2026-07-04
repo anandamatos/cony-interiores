33a6e8dcbe4e88ae4471ecd83377c862cf11429b
Bianca <bianca.rsv27@gmail.com>
2026-07-03 21:55:29 -0300

    feat: implementa CRUD de serviços

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente, Produto, Costureira, Servico, Relatorio, Financeira, Capacidade
 
-# Register your models here.
+admin.site.register(Costureira)
+admin.site.register(Relatorio)
+admin.site.register(Produto)
+admin.site.register(Cliente)
+admin.site.register(Servico)
+admin.site.register(Financeira)
+admin.site.register(Capacidade)
