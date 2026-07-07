671d4180acb8ebb0043294313dfd83e2574c40d6
Matheus <matheusgr2022@outlook.com>
2026-07-07 14:59:56 -0300

    Feat/@Core DISCOVERY-M1-CORE-002/003/004

--- backend/app/core/serializers.py
+++ backend/app/core/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Costureira
+from rest_framework.validators import UniqueValidator
 
 # ----------------------------Adicoes-------------------------------
 
 class CostureiraSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(
+        validators=[UniqueValidator(
+            queryset=Costureira.objects.all(),
+            message="Esta costureira já está cadastrada." #HTTP 400
+        )]
+    )
     class Meta:
         model = Costureira 
         fields = '__all__'
