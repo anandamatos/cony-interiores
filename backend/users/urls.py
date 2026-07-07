d0b24d47bfb97076e5c50ee2b0711ef05130ccda
Bianca <bianca.rsv27@gmail.com>
2026-07-06 21:28:28 -0300

    fix: resolve erros de configuração após integração com Docker

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
 from .views import CostureiraViewSet
@@ -8,3 +9,20 @@ router.register(r'costureiras', CostureiraViewSet, basename='costureira')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+=======
+from django.urls import path
+from .views import hello, home
+from rest_framework.routers import DefaultRouter
+from .views import ServicoViewSet
+
+router = DefaultRouter() #cria as rotas com suas funções: create...
+
+router.register('servico', ServicoViewSet)
+
+urlpatterns = [
+    path('hello/', hello),
+    path('', home),
+]
+
+urlpatterns += router.urls
+>>>>>>> 1703937 (fix: resolve erros de configuração após integração com Docker)
